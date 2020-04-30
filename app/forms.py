25a80973f42a90f15e3c3f7a2c06545afe057865
Ubuntu <ubuntu@ip-172-31-89-155.ec2.internal>
2020-04-30 18:09:13 +0000

    Form was created with description and photo field added.

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,11 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField
+from wtforms import TextAreaField
+from wtforms import SelectField
+from wtforms.validators import DataRequired
+from wtforms.validators import Email
 
+class ProfileForm(FlaskForm):
+    description = TextAreaField('Description', validators = [DataRequired()])
+    photo = FileField('ProfilePic', validators = [FileRequired(),FileAllowed(['jpg','png','jpeg','Images only!'])])
